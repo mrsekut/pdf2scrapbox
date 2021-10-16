c269bd5b40af864410d3f6e900477773dfc6de63
mrsekut <k.cloudspider@gmail.com>
2021-10-16 22:47:45 +0900

    feat: test code

---
+++ test/utils.test.ts
@@ -0,0 +1,27 @@
+import { chunk, range } from 'app/utils';
+
+describe('range', () => {
+  test('range', () => {
+    expect(range(0)).toEqual([]);
+    expect(range(0)).toEqual([]);
+    expect(range(5)).toEqual([0, 1, 2, 3, 4]);
+    expect(range(1, 5)).toEqual([1, 2, 3, 4, 5]);
+    expect(range(3, 5)).toEqual([3, 4, 5]);
+  });
+});
+
+describe('chunks', () => {
+  it('chunk', () => {
+    const array = [1, 2, 3, 4, 5, 6, 7, 8];
+    expect(chunk(array, 1)).toEqual([[1], [2], [3], [4], [5], [6], [7], [8]]);
+    expect(chunk(array, 3)).toEqual([
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8]
+    ]);
+    expect(chunk(array, 5)).toEqual([
+      [1, 2, 3, 4, 5],
+      [6, 7, 8]
+    ]);
+  });
+});
