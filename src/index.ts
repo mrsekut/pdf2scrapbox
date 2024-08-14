b07b92657e529fc043106ffc5ba256f18bcfe669
mrsekut <k.cloudspider@gmail.com>
2024-08-14 14:06:02 +0900

    pdf→imageとimage→jsonを分離

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,8 @@
-import np from 'node:path';
-import Bottleneck from 'bottleneck';
-import cliProgress from 'cli-progress';
 import * as dotenv from 'dotenv';
 
 import { sleep } from 'app/utils/utils.js';
-import {
-  Path,
-  fileInfo,
-  getFileInfo,
-  getImageDirs,
-  getPDFs,
-  mkdir,
-} from 'app/utils/file.js';
-
-import { pdfToImages, pdfs2images, saveFiles } from 'app/pdf.js';
+import { Path, getImageDirs, getImages, getPDFs } from 'app/utils/file.js';
+import { pdfs2images } from 'app/pdf.js';
 import * as Gyazo from './gyazo.js';
 import { renderPage, saveJson } from 'app/renderScrapboxPage.js';
 import { ProfilePath, createProfilePage } from 'app/profilePage.js';
@@ -27,97 +16,49 @@ type Config = {
   profile?: ProfilePath;
 };
 
+// TODO: log
 export async function main(config: Config) {
   const pdfPaths = await getPDFs(config.workspace);
   await pdfs2images(pdfPaths, config.workspace);
 
-  // const dirs = await getImageDirs(config.inputs);
-
-  // const images = [ss, ...dirs];
-  // console.log({ images });
-
-  // for (const pdf of images) {
-  //   const filepath = path.join(config.inputs, pdf);
-  //   console.log({ filepath });
-  //   // await processSinglePDF(config, filepath);
-  // }
+  const dirs = await getImageDirs(config.workspace);
+  await dirs2Cosense(config, dirs);
 }
 
-// // TODO: name, move
-// async function createCosenseJson(config: Config, dirpath: string) {
-//   const pages = await Promise.all(
-//     imgs.map((img, index) =>
-//       limiter.schedule(() => {
-//         progressBar.increment();
-//         return generatePage(img, filename, index, imgs.length, config);
-//       }),
-//     ),
-//   );
-//   //
-// }
+async function dirs2Cosense(config: Config, dirPaths: Path[]) {
+  Promise.all(dirPaths.map(dirPath => dir2Cosense(config, dirPath)));
+}
 
-// async function processSinglePDF(config: Config, filepath: string) {
-//   const { filename } = getFileInfo(filepath, '.pdf');
+async function dir2Cosense(config: Config, dirPath: Path) {
+  const images = await getImages(dirPath);
+  const pages = await Promise.all(
+    images.map((img, index) => {
+      return generatePage(img, index, images.length, config);
+    }),
+  );
 
-//   console.log(`\nProcessing PDF: ${filename}\n`);
+  const pagesWithProfile = await (async () => {
+    if (config.profile == null) return pages;
 
-//   await mkdir(filename);
+    const profilePage = await createProfilePage(config.profile);
+    return [...pages, profilePage];
+  })();
 
-//   const imgs = await pdfToImages(filepath);
+  await saveJson(`${dirPath}-ocr.json`, { pages: pagesWithProfile });
+}
 
-//   const limiter = new Bottleneck({
-//     maxConcurrent: 30,
-//     minTime: 1000,
-//   });
+const generatePage = async (
+  path: Path,
+  index: number,
+  pageLength: number,
+  config: Config,
+) => {
+  const gyazoImageId = await Gyazo.upload(path);
 
-//   const progressBar = new cliProgress.SingleBar(
-//     {},
-//     cliProgress.Presets.shades_classic,
-//   );
+  await sleep(config.waitTimeForOcr);
 
-//   progressBar.start(imgs.length, 0);
+  const ocr = await Gyazo.getGyazoOCR(gyazoImageId);
+  const page = renderPage(index, pageLength, gyazoImageId, ocr);
 
-//   const pages = await Promise.all(
-//     imgs.map((img, index) =>
-//       limiter.schedule(() => {
-//         progressBar.increment();
-//         return generatePage(img, filename, index, imgs.length, config);
-//       }),
-//     ),
-//   );
-
-//   const pagesWithProfile = await (async () => {
-//     if (config.profile == null) return pages;
-
-//     const profilePage = await createProfilePage(config.profile);
-//     return [...pages, profilePage];
-//   })();
-
-//   progressBar.stop();
-
-//   // TODO: out
-//   await saveJson(`out/${filename}-ocr.json`, { pages: pagesWithProfile });
-
-//   console.log(`Finished processing PDF: ${filename}\n`);
-// }
-
-// const generatePage = async (
-//   img: Buffer,
-//   filename: string,
-//   index: number,
-//   pageLength: number,
-//   config: Config,
-// ) => {
-//   // TODO: out
-//   const path = `out/${filename}/${index}.jpg`;
-
-//   const imagePath = await saveFiles(img, path);
-//   const gyazoImageId = await Gyazo.upload(imagePath);
-
-//   await sleep(config.waitTimeForOcr);
-
-//   const ocr = await Gyazo.getGyazoOCR(gyazoImageId);
-//   const page = renderPage(index, pageLength, gyazoImageId, ocr);
-
-//   return page;
-// };
+  return page;
+};
