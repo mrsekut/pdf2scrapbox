40ebf911fbb0a124db0f49367fdc2257f5e57ef9
mrsekut <k.cloudspider@gmail.com>
2024-08-14 12:51:33 +0900

    add getPDFs, getImageDirs

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,6 +1,19 @@
 import fs from 'node:fs/promises';
 import path from 'node:path';
 
+export const getPDFs = async (path_: string) => {
+  const files = await fs.readdir(path_);
+  return files.filter(file => path.extname(file) === '.pdf');
+};
+
+export const getImageDirs = async (path_: string) => {
+  const files = await fs.readdir(path_);
+  return files.filter(async file => {
+    const stats = await fs.lstat(path.join(path_, file));
+    return stats.isDirectory();
+  });
+};
+
 type Extension = '.json' | '.pdf';
 
 export function getFileInfo(filepath: string, extension: Extension) {
