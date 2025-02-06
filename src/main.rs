aa9024a81c906a8d83f926194ce410b27c4d97ff
mrsekut <k.cloudspider@gmail.com>
2025-02-07 00:23:01 +0900

    feat: convert pdfs to images

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+mod pdfs_to_images;
+
+use pdfs_to_images::pdfs_to_images;
+
+#[tokio::main]
+async fn main() {
+    // TODO:
+    let pdfs = vec![PathBuf::from("./workspace/aaa.pdf")];
+    let out_dir = "./workspace";
+
+    match pdfs_to_images(pdfs, out_dir).await {
+        Ok(paths) => println!("Converted PDFs saved to {:?}", paths),
+        Err(e) => eprintln!("Error: {:?}", e),
+    }
 }
