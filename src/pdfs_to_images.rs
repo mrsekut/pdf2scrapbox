c4b6f0767de708034b5c7b8d08f0ccc00a391d27
mrsekut <k.cloudspider@gmail.com>
2025-02-07 00:23:01 +0900

    feat: convert pdfs to images

---
+++ src/pdfs_to_images.rs
@@ -0,0 +1,43 @@
+use std::path::{Path, PathBuf};
+use std::process::Command;
+use tokio::fs;
+use futures::future::join_all;
+
+/// Receives a list of PDFs and outputs images
+pub async fn pdfs_to_images(pdfs: Vec<PathBuf>, out_dir: &str) -> Result<Vec<PathBuf>, Box<dyn std::error::Error>> {
+    let tasks: Vec<_> = pdfs.into_iter().map(|pdf| pdf_to_images(pdf, out_dir.to_string())).collect();
+    let results = join_all(tasks).await;
+    let paths: Vec<_> = results.into_iter().collect::<Result<Vec<_>, _>>()?;
+    Ok(paths)
+}
+
+/// Converts a PDF to images and saves them
+async fn pdf_to_images(pdf: PathBuf, out_dir: String) -> Result<PathBuf, Box<dyn std::error::Error>> {
+    println!("pdf→images: start {:?}", pdf);
+
+    let filename = pdf.file_stem().unwrap().to_string_lossy().into_owned();
+    let out_path = Path::new(&out_dir).join(&filename);
+    fs::create_dir_all(&out_path).await?;
+
+    convert_and_save(&pdf, &out_path).await?;
+
+    println!("pdf→images: end {:?}", pdf);
+    Ok(out_path)
+}
+
+/// Converts a PDF to images using MuPDF's `mutool convert`
+async fn convert_and_save(pdf_path: &PathBuf, out_path: &Path) -> Result<(), Box<dyn std::error::Error>> {
+    let status = Command::new("mutool")
+        .arg("convert")
+        .arg("-F").arg("png")  // Output format
+        .arg("-O").arg("resolution=600,gamma=1") // DPI
+        .arg("-o").arg(out_path.join("%d.png").to_string_lossy().into_owned())  // Output file name (0.png,1.png...)
+        .arg(pdf_path)  // PDF file
+        .status()?;
+
+    if !status.success() {
+        return Err("MuPDF convert failed".into());
+    }
+
+    Ok(())
+}
