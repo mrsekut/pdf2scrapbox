a34a58f1d0a8cfe8390d5b77c0d396423fb4fa22
mrsekut <k.cloudspider@gmail.com>
2025-02-15 01:41:19 +0900

    move

---
+++ src/files.rs
@@ -0,0 +1,49 @@
+use std::{fs, path::PathBuf};
+
+pub fn get_pdf_paths(path: &PathBuf) -> Result<Vec<PathBuf>, Box<dyn std::error::Error>> {
+    let paths = fs::read_dir(path)?
+        .filter_map(|entry| {
+            let path = entry.ok()?.path();
+
+            if path.extension()?.eq_ignore_ascii_case("pdf") {
+                Some(path)
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<PathBuf>>();
+
+    Ok(paths)
+}
+
+pub fn get_image_dirs(path: &PathBuf) -> Result<Vec<PathBuf>, Box<dyn std::error::Error>> {
+    let paths = fs::read_dir(path)?
+        .filter_map(|entry| {
+            let path = entry.ok()?.path();
+
+            if path.is_dir() {
+                Some(path)
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<PathBuf>>();
+
+    Ok(paths)
+}
+
+pub fn get_images(path: &PathBuf) -> Result<Vec<PathBuf>, Box<dyn std::error::Error>> {
+    let paths = fs::read_dir(path)?
+        .filter_map(|entry| {
+            let path = entry.ok()?.path();
+
+            if path.extension()?.eq_ignore_ascii_case("png") {
+                Some(path)
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<PathBuf>>();
+
+    Ok(paths)
+}
