23b2f0df460d0241ea8c6bbf973e31bacde7c6fd
mrsekut <k.cloudspider@gmail.com>
2025-02-15 19:59:43 +0900

    refactor: Config to use Arc for shared ownership and improve async handling

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ use gyazo_api::{upload::GyazoUploadOptions, Gyazo};
 use pdfs_to_images::pdfs_to_images;
 use render_page::{create_profile_page, render_page, save_json, Page, Project};
 use std::{
+    env,
     path::{Path, PathBuf},
+    sync::Arc,
     time::Duration,
 };
 use tokio::{task, time::sleep};
@@ -14,27 +16,29 @@ mod files;
 mod pdfs_to_images;
 mod render_page;
 
-#[derive(Clone)]
 struct Config {
     wait_time_for_ocr: Duration,
     workspace_dir: PathBuf,
     profile: Option<String>,
+    gyazo: Gyazo,
 }
 
 impl Config {
     fn new() -> Result<Self, Box<dyn std::error::Error>> {
+        dotenv().ok();
+        let gyazo_token = env::var("GYAZO_TOKEN")?;
         Ok(Self {
             wait_time_for_ocr: Duration::from_millis(5000),
             workspace_dir: PathBuf::from("./workspace"),
             profile: Some("mrsekut-merry-firends/mrsekut".to_string()),
+            gyazo: Gyazo::new(gyazo_token),
         })
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    dotenv().ok();
-    let config = Config::new()?;
+    let config = Arc::new(Config::new()?);
     let pdf_paths = get_pdf_paths(&config.workspace_dir)?;
 
     match pdfs_to_images(pdf_paths, &config.workspace_dir).await {
@@ -43,22 +47,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     };
 
     let dirs = get_image_dirs(&config.workspace_dir)?;
-    dirs_to_cosense(&config, &dirs).await;
+    dirs_to_cosense(config.clone(), &dirs).await;
 
     Ok(())
 }
 
-async fn dirs_to_cosense(config: &Config, dir_paths: &[PathBuf]) {
+async fn dirs_to_cosense(config: Arc<Config>, dir_paths: &[PathBuf]) {
     let tasks: Vec<_> = dir_paths
         .iter()
-        .map(|dir| dir_to_cosense(config, dir))
+        .map(|dir| dir_to_cosense(config.clone(), dir))
         .collect();
     future::join_all(tasks).await;
 }
 
 // TODO: clean, warning, cli, dos
 async fn dir_to_cosense(
-    config: &Config,
+    config: Arc<Config>,
     dir_path: &PathBuf,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let images = get_images(dir_path)?;
@@ -68,9 +72,9 @@ async fn dir_to_cosense(
         .into_iter()
         .enumerate()
         .map(|(index, image)| {
-            let config = config.clone();
+            let config = Arc::clone(&config);
             task::spawn(async move {
-                generate_page(&config, index, image.clone(), total_pages)
+                generate_page(config, index, image.clone(), total_pages)
                     .await
                     .ok()
             })
@@ -101,19 +105,21 @@ async fn dir_to_cosense(
 
 // TODO: retry?
 async fn generate_page(
-    config: &Config,
+    config: Arc<Config>,
     index: usize,
     pdf_path: PathBuf,
     page_num: usize,
 ) -> Result<Page, Box<dyn std::error::Error>> {
     let options = GyazoUploadOptions::default();
-    let gyazo_token = std::env::var("GYAZO_TOKEN").expect("GYAZO_ACCESS_TOKEN must be set");
-    let gyazo = Gyazo::new(gyazo_token);
-    let gyazo_image_id = gyazo.upload(pdf_path, Some(&options)).await?.image_id;
+    let gyazo_image_id = config
+        .gyazo
+        .upload(pdf_path, Some(&options))
+        .await?
+        .image_id;
 
     sleep(config.wait_time_for_ocr).await;
 
-    let ocr_text = gyazo.image(&gyazo_image_id).await?.ocr.description;
+    let ocr_text = config.gyazo.image(&gyazo_image_id).await?.ocr.description;
     let page = render_page(index, page_num, &gyazo_image_id, &ocr_text);
 
     println!("Processed page: {index}");
